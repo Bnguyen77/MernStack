098fe35cf8844d5080d1944a40887dc8ed074f06
BinhNg <bng7761@gmail.com>
2023-01-10 09:37:46 -0600

    finishing on route,(gitapi problem still)

--- devConnector/routes/api/profile.js
+++ devConnector/routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
@@ -316,6 +318,36 @@ router.delete('/education/:education_id', auth, async (req, res) => {
     await profile.save();
     res.json(profile);
   } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+//-------------------------------------------------------------------------
+// @route   GET api/profile/github/:username
+// @desc    get user repos from Github
+// @access  Public
+
+router.get('/github/:username', (req, res) => {
+  try {
+    const ghci = config.get('githubClientId');
+    const ghs = config.get('githubSecret');
+    const options = {
+      uri: "https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}" ,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No Github profile found' });
+      }
+      res.json(JSON.parse(body));
+    });
+  }
+  catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
   }
