228038ec4478e6afb45646f12fd3e08d290bd5f0
BinhNg <bng7761@gmail.com>
2023-01-12 13:04:56 -0600

    set up react

---
+++ devConnector/client/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import './App.css';
+
+const App = () => (
+	<Fragment>
+		<h1>App</h1>
+	</Fragment>
+);
+
+
+
+export default App;
