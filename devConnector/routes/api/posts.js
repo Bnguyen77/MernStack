6e3afda2bfaecce434af0e50258bf6559c43ddb3
BinhNg <bng7761@gmail.com>
2023-01-12 00:19:38 -0600

    comment -post/delete/edit

--- devConnector/routes/api/posts.js
+++ devConnector/routes/api/posts.js
@@ -92,6 +92,41 @@ router.post(
 );
 
 //-------------------------------------------------------------------------
+// @route POST api/posts/comment/:post_id
+// @desc create comment for post
+// @access Private
+
+router.post(
+	'/comment/:post_id',
+	[auth, [check('text', 'text is required').not().isEmpty()]], // check text if empty
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		try {
+			const post = await Post.findById(req.params.post_id);
+			const user = await User.findById(req.user.id).select('-password');
+
+			const newComment = {
+				text: req.body.text,
+				name: user.name,
+				avatar: user.avatar,
+				user: req.user.id,
+			};
+
+			post.comments.unshift(newComment);
+
+			await post.save();
+			res.json(post.comments);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
+	}
+);
+
+//-------------------------------------------------------------------------
 // @route   DELETE api/posts/:post_id
 // @desc    Delete post by post id
 // @access  Private
@@ -115,6 +150,45 @@ router.delete('/:post_id', auth, async (req, res) => {
 		if (err.kind === 'ObjectId') {
 			return res.status(404).json({ msg: 'post not found' });
 		}
+		res.status(500).send('Server Error');
+	}
+});
+
+//-------------------------------------------------------------------------
+// @route   DELETE api/posts/comment/:post_id/:comment_id
+// @desc    Delete comment by post_id
+// @access  Private
+
+router.delete('/comment/:post_id/:comment_id', auth, async (req, res) => {
+	try {
+		// get post
+		const post = await Post.findById(req.params.post_id);
+		//get comment that match comments id
+		const comment =  post.comments.find(
+			(comment) => comment.id === req.params.comment_id
+		);
+
+		//check if comment exist
+		if (!comment) {
+			return res.status(404).json({ msg: 'comment not found' });
+		}
+
+		//check if comment belong to login user
+		if (comment.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: 'unauthorized user' });
+		}
+		//find index from id
+		const removeIndex = post.comments
+			.map(comment => comment.user.toString())
+			.indexOf(req.user.id);
+		// remove
+		post.comments.splice(removeIndex, 1);
+
+		await post.save();
+		
+		res.json(post.comments);
+	} catch (err) {
+		console.error(err.message);
 		res.status(500).send('Server Error');
 	}
 });
@@ -189,21 +263,59 @@ router.put('/unlike/:post_id', auth, async (req, res) => {
 
 		//check if the post has already been like by this user
 		if (
-			post.likes.filter(like => like.user.toString() === req.user.id).length ===
-			0
+			post.likes.filter((like) => like.user.toString() === req.user.id)
+				.length === 0
 		) {
 			return res.status(400).json({ msg: 'Post has not yet been liked' });
 		}
-		
-        // get remove index
-        const removeIndex = post.likes
-			.map(like => like.user.toString())
+
+		// get remove index
+		const removeIndex = post.likes
+			.map((like) => like.user.toString())
 			.indexOf(req.user.id);
 		// remove
 		post.likes.splice(removeIndex, 1);
-        
-        await post.save();
+
+		await post.save();
 		res.json(post.likes);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
+//-------------------------------------------------------------------------
+// @route   Put api/posts/comment/:post_id/:comment_id
+// @desc   edit comment from post
+// @access  Private
+
+router.put('/comment/:post_id/:comment_id', auth, async (req, res) => {
+	try {
+		// get post
+		const post = await Post.findById(req.params.post_id);
+		//get comment that match comments id
+		const comment =  post.comments.find(
+			(comment) => comment.id === req.params.comment_id
+		);
+
+		//check if comment exist
+		if (!comment) {
+			return res.status(404).json({ msg: 'comment not found' });
+		}
+
+		//check if comment belong to login user
+		if (comment.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: 'unauthorized user' });
+		}
+		//find index from id
+        comment.text = req.body.text;
+		// const removeIndex = post.comments
+		// 	.map(comment => comment.user.toString())
+		// 	.indexOf(req.user.id);
+		// remove
+
+		await post.save();
+		res.json(post.comments);
 	} catch (err) {
 		console.error(err.message);
 		res.status(500).send('Server Error');
