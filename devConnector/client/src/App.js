3a88ebad071647945388e968ee23deb2d70c2900
BinhNg <bng7761@gmail.com>
2023-01-12 19:35:59 -0600

    add layout/auth pages

--- devConnector/client/src/App.js
+++ devConnector/client/src/App.js
@@ -1,13 +1,32 @@
-// import logo from './logo.svg';
 import React, { Fragment } from 'react';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
 const App = () => (
-	<Fragment>
-		<h1>App</h1>
-	</Fragment>
+	<Router>
+		<Fragment>
+			<Navbar />
+			<Routes>
+				<Route exact path="/" element={<Landing/>} /> 
+			</Routes>
+      <section className = "container">
+       
+        <Routes>
+				<Route exact path="/register" element={<Register/>} /> 
+				<Route exact path="/login" element={<Login/>} /> 
+			  </Routes>
+        
+      </section>
+		</Fragment>
+	</Router>
 );
-
-
 
 export default App;
---
+++ devConnector/client/src/components/auth/Login.js
@@ -0,0 +1,34 @@
+import React, {Fragment} from 'react'
+
+const Login = () => {
+  return <Fragment>
+        <div className="alert alert-danger">
+  Invalid credentials
+</div>
+<h1 className="large text-primary">Sign In</h1>
+<p className="lead"><i className="fas fa-user"></i> Sign into Your Account</p>
+<form className="form" action="dashboard.html">
+  <div className="form-group">
+    <input
+      type="email"
+      placeholder="Email Address"
+      name="email"
+      required
+    />
+  </div>
+  <div className="form-group">
+    <input
+      type="password"
+      placeholder="Password"
+      name="password"
+    />
+  </div>
+  <input type="submit" className="btn btn-primary" value="Login" />
+</form>
+<p className="my-1">
+  Don't have an account? <a href="register.html">Sign Up</a>
+</p>
+</Fragment>
+}
+
+export default Login
