be03877db7cef9bf6ba0b65850bbf5a1373073eb
BinhNg <bng7761@gmail.com>
2023-01-13 16:00:05 -0600

    ALOT OF REDUX

--- devConnector/client/src/App.js
+++ devConnector/client/src/App.js
@@ -1,32 +1,36 @@
 import React, { Fragment } from 'react';
-import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
+
 const App = () => (
-	<Router>
-		<Fragment>
-			<Navbar />
-			<Routes>
-				<Route exact path="/" element={<Landing/>} /> 
-			</Routes>
-      <section className = "container">
-       
-        <Routes>
-				<Route exact path="/register" element={<Register/>} /> 
-				<Route exact path="/login" element={<Login/>} /> 
-			  </Routes>
-        
-      </section>
-		</Fragment>
-	</Router>
+	<Provider store = {store}>
+		<Router>
+			<Fragment>
+				<Navbar />
+				<Routes>
+					<Route exact path='/' element={<Landing />} />
+				</Routes>
+				<Alert/>
+				<section className='container'>
+					<Routes>
+						<Route exact path='/register' element={<Register />} />
+						<Route exact path='/login' element={<Login />} />
+					</Routes>
+				</section>
+			</Fragment>
+		</Router>
+	</Provider>
 );
 
 export default App;
